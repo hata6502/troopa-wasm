69bac4b20fd1cd4b6d7e5849241f6802b3451fcd
Tomoyuki Hata <hato6502@gmail.com>
2020-10-17 21:02:53 +0900

    the first rust program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).unwrap();
+
+    println!("Hello, world!: {}", guess);
 }
