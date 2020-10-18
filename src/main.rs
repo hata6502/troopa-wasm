96a91619512348071d8cc9737583e7e3a339080e
Tomoyuki Hata <hato6502@gmail.com>
2020-10-19 02:03:59 +0900

    learning enum, struct, sharing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::io;
+mod synthesizer_core {
+    const COMPONENT_IN_PORT_LENGTH: usize = 8;
+    const COMPONENT_REGISTER_LENGTH: usize = 8;
+
+    pub enum ComponentType {
+        Mixer,
+        Sine,
+    }
+
+    pub struct Component<'a> {
+        component_type: ComponentType,
+        in_ports: [InPort<'a>; COMPONENT_IN_PORT_LENGTH],
+        out_port: OutPort<'a>,
+        registers: [f64; COMPONENT_REGISTER_LENGTH],
+    }
+
+    struct InPort<'a> {
+        component: &'a Component<'a>,
+        out_port: &'a OutPort<'a>,
+    }
+
+    struct OutPort<'a> {
+        pub in_ports: Vec<&'a InPort<'a>>,
+        value: f64,
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).unwrap();
-
-    println!("Hello, world!: {}", guess);
+    let component = synthesizer_core::Component {
+        component_type: synthesizer_core::ComponentType::Mixer,
+        in_ports: 0,
+        out_port: 0,
+        registers: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    };
 }
