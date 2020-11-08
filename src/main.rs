7ed7608924ee2a609be05f914dae585e84296d88
Tomoyuki Hata <hato6502@gmail.com>
2020-11-08 21:17:33 +0900

    implement propagation currently

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,35 @@ use synthesizer_core::*;
 fn main() {
     let mut components = Vec::new();
 
+    components.push(Rc::new(RefCell::new(Component::new(
+        ComponentType::NoOperation,
+    ))));
     components.push(Rc::new(RefCell::new(Component::new(ComponentType::Mixer))));
     components.push(Rc::new(RefCell::new(Component::new(ComponentType::Mixer))));
 
     Component::connect(
-        2,
-        Rc::downgrade(&components[0]),
+        0,
         Rc::downgrade(&components[1]),
+        Rc::downgrade(&components[0]),
     );
+
     Component::connect(
-        2,
+        1,
         Rc::downgrade(&components[1]),
         Rc::downgrade(&components[0]),
     );
+
+    Component::connect(
+        0,
+        Rc::downgrade(&components[2]),
+        Rc::downgrade(&components[1]),
+    );
+
+    Component::connect(
+        1,
+        Rc::downgrade(&components[2]),
+        Rc::downgrade(&components[1]),
+    );
+
+    components[0].borrow_mut().set_output(1.0);
 }
