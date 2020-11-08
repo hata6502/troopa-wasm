745bbffdf578011ecf3011844a2a433f97fb3682
Tomoyuki Hata <hato6502@gmail.com>
2020-11-08 21:17:33 +0900

    implement propagation currently

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,14 @@ use std::rc::Weak;
 
 pub enum ComponentType {
     Mixer,
+    NoOperation,
     Sine,
 }
 
 pub struct Component {
     component_type: ComponentType,
     inputs: [Option<Weak<RefCell<Component>>>; COMPONENT_INPUT_LENGTH],
+    output: f64,
     outputs: Vec<Weak<RefCell<Component>>>,
     registers: [f64; COMPONENT_REGISTER_LENGTH],
 }
@@ -23,6 +25,7 @@ impl Component {
         Component {
             component_type,
             inputs: [INPUT; COMPONENT_REGISTER_LENGTH],
+            output: 0.0,
             outputs: Vec::new(),
             registers: [0.0; COMPONENT_REGISTER_LENGTH],
         }
@@ -33,8 +36,25 @@ impl Component {
         input: Weak<RefCell<Component>>,
         output: Weak<RefCell<Component>>,
     ) {
-        // TODO: disconnect prev connection
         input.upgrade().unwrap().borrow_mut().inputs[index] = Some(Weak::clone(&output));
         output.upgrade().unwrap().borrow_mut().outputs.push(input);
+    }
+
+    pub fn set_output(&mut self, output: f64) {
+        println!("{0}", output);
+
+        self.output = output;
+
+        for output in &self.outputs {
+            if let Some(output) = output.upgrade() {
+                output.borrow_mut().sync();
+            }
+        }
+    }
+
+    fn sync(&mut self) {
+        // TODO: implement each component behavior
+        // TODO: use queue instead of stack
+        self.set_output(1.0);
     }
 }
