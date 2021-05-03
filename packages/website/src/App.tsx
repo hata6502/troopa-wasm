8d2ae104f20ba7bab02074c8a27469db0ce9a0fb
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-05-03 13:00:01 +0900

    Set up ESLint

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -5,41 +5,38 @@ import type { FunctionComponent } from "react";
 const App: FunctionComponent = memo(() => {
   const [audioContext, setAudioContext] = useState<AudioContext>();
 
-  const handlePlayButtonClick = useCallback(()=> {
+  const handlePlayButtonClick = useCallback(() => {
     const audioContext = new AudioContext();
     const scriptNode = audioContext.createScriptProcessor(undefined, 0, 1);
-  
-    scriptNode.addEventListener('audioprocess', (event) => {
+
+    scriptNode.addEventListener("audioprocess", (event) => {
       const channelData = event.outputBuffer.getChannelData(0);
-  
+
       for (let i = 0; i < channelData.length; i++) {
         channelData[i] = Math.sin(i++);
       }
     });
-  
+
     scriptNode.connect(audioContext.destination);
 
     setAudioContext(audioContext);
   }, []);
 
-
-  const handleStopButtonClick = useCallback(()=> 
-    audioContext?.close()
-  , []);
+  const handleStopButtonClick = useCallback(() => audioContext?.close(), []);
 
   return (
     <>
       <AppBar color="inherit" position="sticky">
         <Toolbar>
           <Grid container spacing={2} alignItems="baseline">
-              <Grid item>
-                <Typography variant="h6">troopa</Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography variant="subtitle1">web toy synthesizer</Typography>
-              </Grid>
+            <Grid item>
+              <Typography variant="h6">troopa</Typography>
             </Grid>
+
+            <Grid item>
+              <Typography variant="subtitle1">web toy synthesizer</Typography>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
 
